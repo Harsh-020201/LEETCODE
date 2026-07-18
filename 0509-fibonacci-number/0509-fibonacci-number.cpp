ddbb3826cc5562c595158168c1caf9c8046923f9
HARSH VIMAL <harsh.vimal02@gmail.com>
2026-07-18 12:18:04 +0530

    Time: 9 ms (49.25%), Space: 7.8 MB (65.24%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        int sum=0;
+        if(n<0){
+            return false;
+        }
+        if(n==0 ){
+            return 0;
+        }
+        if(n==1 ){
+            return 1;
+        }
+        else{
+            return fib(n-1)+fib(n-2);
+            }
+        }
+    
+};
